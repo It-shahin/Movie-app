225119d68e601dbbbffb6868b345ac9ea887f9c9
Chahin Boudra <boudrachahin@gmail.com>
2026-03-05 18:48:49 +0000

    chore: add Tailwind CSS and Vite dependencies to package.json

---
+++ movie-app/src/components/search.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+const search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className='search'>
+        <div>
+            <img src="./src/assets/search.svg" alt="Search" />
+
+            <input
+                type="text"
+                placeholder='Search for movies...'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            
+        </div>
+    </div>
+  )
+}
+
+export default search
